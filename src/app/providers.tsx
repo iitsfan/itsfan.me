bb827df423595b851e7bb0c8cd22a516af91dd13
FAN <i@itsfan.me>
2025-09-27 01:20:14 +0800

    chore(deps): remove lenis

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { ProgressProvider } from '@bprogress/next/app'
-import { ReactLenis } from 'lenis/react'
 import { ThemeProvider } from 'next-themes'
 import '@/styles/globals.scss'
 
@@ -17,7 +16,6 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
 				disableStyle
 				shallowRouting
 			>
-				<ReactLenis root />
 				{children}
 			</ProgressProvider>
 		</ThemeProvider>
