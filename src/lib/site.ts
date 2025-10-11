94758a0ded50414a3870058418d9f2b3e33fc67f
FAN <i@itsfan.me>
2025-10-11 17:03:22 +0800

    feat: add site configs

---
+++ src/lib/site.ts
@@ -0,0 +1,21 @@
+const SITE_URL = 'https://itsfan.me'
+const OG_IMAGE = `${SITE_URL}/avatar.jpeg`
+
+export const siteConfig = {
+	title: 'FAN',
+	tagline: 'Keep Running...',
+	description: 'FAN\'S BLOG',
+	url: SITE_URL,
+	locale: 'zh_CN',
+	keywords: ['FAN', 'itsfan', 'itsfan.me'],
+	author: {
+		name: 'FAN',
+		email: 'i@itsfan.me',
+		url: SITE_URL,
+	},
+	ogImage: {
+		url: OG_IMAGE,
+		width: 460,
+		height: 460,
+	},
+}
