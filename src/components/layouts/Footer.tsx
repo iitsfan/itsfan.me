8e2a93199d673fb787b70eb8bd9bd84ee5c030b9
FAN <i@itsfan.me>
2025-10-15 19:56:47 +0800

    feat: add language switch button to footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,13 +1,17 @@
+import LanguageSwitch from '@/components//LanguageSwitch'
 import ToggleThemeButton from '@/components//toggleThemeButton'
 
 export default function Footer() {
 	return (
 		<footer className="mb-2 flex w-full items-center justify-center px-4 sm:px-0">
-			<div className="flex max-w-xl flex-1 items-center gap-x-3">
+			<div className="flex max-w-xl flex-1 items-center justify-between gap-x-3">
 				<p className="block font-bold text-(--text-primary)">
 					© 2025 – FAN
 				</p>
-				<ToggleThemeButton />
+				<div className="flex items-center gap-x-0.5">
+					<LanguageSwitch />
+					<ToggleThemeButton />
+				</div>
 			</div>
 		</footer>
 	)
